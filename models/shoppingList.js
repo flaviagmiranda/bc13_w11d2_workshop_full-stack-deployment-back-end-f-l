e6e9ea3c4f42d8c496924db2bc7a1ef2fd58d4fa
flaviamiranda <flaviagracianom@gmail.com>
2022-12-06 08:48:27 -0300

    creating routes and models

--- models/shoppingList.js
+++ models/shoppingList.js
@@ -1,19 +1,27 @@
-import { pool } from "../db/index.js";
+import { pool } from '../db/index.js'
 
-export async function getShoppingList() {
-  const data = await pool.query("SELECT * FROM shopping;");
-  console.log("The shopping list is", data.rows);
-  return data.rows;
+export async function getShoppingList () {
+  const data = await pool.query('SELECT * FROM shopping;')
+  console.log('The shopping list is', data.rows)
+  return data.rows
 }
 
-export async function postListItem(listItem) {
-  const { item, completed } = listItem;
+export async function postListItem (listItem) {
+  const { item, completed } = listItem
   const data = await pool.query(
     `INSERT INTO shopping (
       item,
       completed
     ) VALUES ($1,$2) RETURNING *;`,
     [item, completed]
-  );
-  return data.rows[0];
+  )
+  return data.rows[0]
+}
+
+export async function editListItem (itemId) {
+  const result = await pool.query(
+    'UPDATE shopping SET completed = NOT completed WHERE id = $1 RETURNING *',
+    [itemId]
+  )
+  return result.rows[0]
 }
--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -1,18 +1,28 @@
-import express from "express";
-import { getShoppingList, postListItem } from "../models/shoppingList.js";
+import express from 'express'
+import {
+  getShoppingList,
+  postListItem,
+  editListItem
+} from '../models/shoppingList.js'
 
-const router = express.Router();
+const router = express.Router()
 
 /* GET shopping list. */
-router.get("/", async (req, res) => {
-  const data = await getShoppingList();
-  res.json({ success: true, payload: data });
-});
+router.get('/', async (req, res) => {
+  const data = await getShoppingList()
+  res.json({ success: true, payload: data })
+})
 
-router.post("/", async (req, res) => {
-  const { listItem } = req.body;
-  const result = await postListItem(listItem);
-  res.status(201).json({ success: true, payload: result });
-});
+router.post('/', async (req, res) => {
+  const { listItem } = req.body
+  const result = await postListItem(listItem)
+  res.status(201).json({ success: true, payload: result })
+})
 
-export default router;
+router.patch('/:id', async function (req, res) {
+  const id = req.params.id
+  const editListItem = await editListItem(id)
+  res.json({ success: true, payload: editListItem })
+})
+
+export default router
